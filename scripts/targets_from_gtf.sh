d006761e37c75f0771d86d63af29bde2eb91cbc5
Adam Faranda <adam.faranda@gmail.com>
2020-08-25 11:26:45 -0400

    Updated targets_from_gtf.sh: added explanatory comments

--- scripts/targets_from_gtf.sh
+++ scripts/targets_from_gtf.sh
@@ -3,7 +3,17 @@
 #SBATCH --mem=32000
 #SBATCH --ntasks=4
 #
-
+# File: targets_from_gtf.sh
+# Purpose: Extract lines from a GTF file by matching an attribute
+# Created: August 25, 2020
+# Author: Adam Faranda
+#
+# Set Environment Variables
+#  TARGETS= << Path to file listing Target ID's or Symbols in single column >>
+#  TGTLABL= << Label appended to target ID, eg gene_id, gene_name etc. >>
+#  GTFPATH= << Lines extracted from the file at this path >>
+#  RESULTS= << Output stored in the file at this path >>
+#
 
 TARGETS=/work/abf/MouseEnsembl100/Crystallin_symbols.txt
 TGTLABL=gene_name
@@ -16,7 +26,7 @@ then
 	 -v FS="\t| |;"\
 	 '\
           # Store values from first file in an array of label target pairs
-          # with the target surrounded by quotes using \042
+          # with the target surrounded by quotes using \042 
 	  (NR == FNR)\
           {
               tgt[lbl" \042"$1"\042"];
