f3ac5acac2c9763fd2d5b52f42c1f3e2f21a98bc
Adam Faranda <adam.faranda@gmail.com>
2020-10-06 22:04:14 -0400

    Started working on Single Ended Pipeline

---
+++ scripts/Align_Single_End.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# File: Process_Paired_End.sh
+# Author: Adam Faranda
+# Purpose:
+#     Align single end RNASeq reads to a refference genome and quantify
+#     gene / transcript abundance
+# Created: September 21, 2020
+
+
+
+# Define Main Directories
+export PATH=${PATH}:$(pwd)/scripts  # Add this pipeline to the executeable path
+export FASTQDIR=$(pwd)/fastq        # Path to directory with reads
+export HISAT2_INDEXES=/work/abf/MouseEnsembl100                    # Path to Genome Index Directory
+export HISAT2_PREFIX=genome_tran                              # Prefix for Hisat2 index
+export ALIGNDIR=$(pwd)/Alignments   # Path to alignment output directory
+export COUNTDIR=$(pwd)/Counts       # Path to count output directory
+export STRNGDIR=$(pwd)/Stringtie    # Path to Stringtie output directory
+export PRETRIM_QC=$(pwd)/PRE_FastQC      # Path to Pre Trim QC Output Directory
+export POSTTRIM_QC=$(pwd)/POST_FastQC # Path to Post Trimming FastQC(unify later)
+export GTFPATH=/work/abf/MouseEnsembl100/Mus_musculus.GRCm38.100.gtf    # Path to GTF File
+export RSEQCDIR=$(pwd)/RSeQC_Results        # Path to RSeQC results
+export BEDPATH=/work/abf/MouseEnsembl100/rseqc_gene_models.bed # Path to bed file for RSeQC
+export TRIMDIR=$(pwd)/Trimmed   # Path to Trimmed Reads Directory
+export FQTARGET="L[0-9]\{3\}_R1_[0-9]\{3\}\.fastq\.gz"  # Regex for fastq files
+export DELOLD=0 # Set to 1 to delete previous results
+
+# De-Gitify and Create directories if none exist
+if [ -d .git ]; then
+    rm -rf .git
+fi
+
+if [ -f .gitignore ]; then
+   rm .gitignore
+fi
+
+ARR=($ALIGNDIR \
+	 $COUNTDIR \
+	 $STRNGDIR \
+	 $PRETRIM_QC \
+	 $POSTTRIM_QC \
+	 $TRIMDIR \
+	 $RSEQCDIR)
+for D in ${ARR[@]}; do
+    if [ ! -d $D ]; then
+	mkdir $D
+	
+    elif [ $DELOLD == 1 ]; then
+	rm -rf $D
+	mkdir $D
+    fi
+done
+    
+# Iterate over FastQ Files, launch aprocessing script for each pair of reads
+echo $(ls -lh $FASTQDIR)
+JOBS=""
+for R1 in $(ls $FASTQDIR | grep $FQTARGET)
+do
+    # Align Single end reads to genome and quantify abundance
+    # R2=$(echo $R1 | sed 's/R1/R2/')
+    echo "Running" sbatch SE_Hisat2_Htseq_Stringtie.sh $R1
+    JB=$(sbatch SE_Hisat2_Htseq_Stringtie.sh $R1 | gawk '{print $4}')
+    JOBS=${JOBS},afterok:${JB}
+
+    # Align Paired end reads to Ribosomal DNA fragment Rn45s
+    RB=$(sbatch\
+	     --dependency=afterok:${JB}\
+	     PE_ribosomal_content_analysis.sh $R1 | gawk '{print $4}'
+      )
+    ROBS=${ROBS},afterok:${RB}
+done
+
+# Aggregate QC Results in One Place
+export JOBS=$(echo $JOBS | sed 's/,//')
+export ROBS=$(echo $ROBS | sed 's/,//')
+sbatch --dependency=${JOBS},${ROBS} RunQC.sh
---
+++ scripts/SE_Hisat2_Htseq_Stringtie.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#SBATCH --job-name=PE_Hisat2_Htseq_Stringtie
+#SBATCH --mem=128000
+#SBATCH --ntasks=8
+
+ID=$(echo $1 | sed 's/_L[0-9]\{3\}_R[12]_[0-9]\{3\}\.fastq\.gz//')
+F1=$(echo $1 | sed 's/\.fastq\.gz/_fastqc.html/')
+echo $ID
+
+## Pretrim FastQC
+if [ -f ${PRETRIM_QC}/${F1} ]
+then
+    echo skipping pretrim FastQC for $1
+else
+    fastqc ${FASTQDIR}/${1} -o ${PRETRIM_QC}
+fi
+
+## Get Trimmed Filenames
+R1=$(echo $1 | sed 's/\.fastq\.gz/_val_1.fq.gz/')
+
+## Trimgalore Adjust parameters depending on
+## kit used (add clipping and change length for smarter stranded)
+if [ -f ${TRIMDIR}/${R1} ]
+then
+    echo skipping trim for $ID
+else
+    trim_galore -o $TRIMDIR \
+		--length 35 \
+		--phred33 \
+		${FASTQDIR}/${1}
+fi
+
+## Run Posttrim FastQC
+F1=$(echo $R1 | sed 's/\.fq\.gz/_fastqc.html/')
+if [ -f ${POSTTRIM_QC}/${F1} ]
+then
+    echo skipping posttrim QC $R1
+else
+    fastqc ${TRIMDIR}/${R1} -o ${POSTTRIM_QC}
+fi
+
+## Align Trimmed Reads Using Hisat2
+if [ -f ${ALIGNDIR}/${ID}_sorted_alignment.bam ]
+then
+   echo skipping alignment for sample $ID
+else
+    hisat2 -p8\
+	   --verbose\
+	   --phred33\
+	   --dta\
+	   --fr\
+	   -x $HISAT2_PREFIX\
+	   -U ${TRIMDIR}/${R1}\
+	   -S ${ALIGNDIR}/${ID}_aligned_reads.sam
+
+    ## compress, sort, and index alignments
+    samtools view -bS ${ALIGNDIR}/${ID}_aligned_reads.sam > ${ALIGNDIR}/${ID}_aligned_reads.bam
+    
+    samtools sort -o ${ALIGNDIR}/${ID}_sorted_alignment.bam ${ALIGNDIR}/${ID}_aligned_reads.bam
+    
+    samtools index ${ALIGNDIR}/${ID}_sorted_alignment.bam
+    
+    rm ${ALIGNDIR}/${ID}_aligned_reads.bam ${ALIGNDIR}/${ID}_aligned_reads.sam
+fi
+
+## Get Gene-level counts with Htseq Count
+# Call htseq-count on the target bam file
+if [ -f ${COUNTDIR}/${ID}_GeneCount.txt ]
+then
+    echo skipping htseq-count for $ID
+else
+    htseq-count \
+	-i gene_id -r pos -f bam -s reverse -m union --type exon \
+	${ALIGNDIR}/${ID}_sorted_alignment.bam \
+	$GTFPATH > ${COUNTDIR}/${ID}_GeneCount.txt
+fi
+
+# Call stringtie on the target bam file
+if [ -f ${STRNGDIR}/${ID}/${ID}.txt ]
+then
+    echo skipping stringtie for $ID
+else
+    stringtie ${ALIGNDIR}/${ID}_sorted_alignment.bam -p8\
+	      --fr\
+	      -e -B\
+	      -G $GTFPATH\
+	      -o ${STRNGDIR}/${ID}/${ID}.gtf\
+	      -A ${STRNGDIR}/${ID}/${ID}.txt
+fi
