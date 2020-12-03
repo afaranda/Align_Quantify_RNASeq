ebd87d4fdc73b5db3662ad7b6b9edba59555afb3
Adam Faranda <adam.faranda@gmail.com>
2020-12-03 10:09:28 -0500

    PE_rib*sis.sh: revised fraction counting logic

--- scripts/PE_ribosomal_content_analysis.sh
+++ scripts/PE_ribosomal_content_analysis.sh
@@ -26,9 +26,9 @@ else
 	   -S ${ALIGNDIR}/${ID}_ribo_reads.sam
 
     ## compress, sort, and index alignments
-    samtools view -bS ${ALIGNDIR}/${ID}_ribo_reads.sam > ${ALIGNDIR}/${ID}_ribo_reads.bam
+    samtools view -@ 8 -bS ${ALIGNDIR}/${ID}_ribo_reads.sam > ${ALIGNDIR}/${ID}_ribo_reads.bam
     
-    samtools sort -o ${ALIGNDIR}/${ID}_sorted_ribo.bam ${ALIGNDIR}/${ID}_ribo_reads.bam
+    samtools sort -@ 8 -m 12G -o ${ALIGNDIR}/${ID}_sorted_ribo.bam ${ALIGNDIR}/${ID}_ribo_reads.bam
     
     samtools index ${ALIGNDIR}/${ID}_sorted_ribo.bam
     
@@ -81,36 +81,137 @@ gawk -v FS="\t"\
         }
      }
     {rc = FNR - 1}
-    (fc == 0){aln[$1]}	
-    (fc == 1){unl[$1]}	
+    (fc == 0){all[$1]}	
+    (fc == 1){gen[$1]}	
     (fc == 2){rib[$1]}	
     
     END\
     {
-        alnCount = 0
-        unlCount = 0
+        allCount = 0
+        genCount = 0
         ribCount = 0
         nulCount = 0
+        inxCount = 0
 
-        for(read in rib)
+        for(read in all)
         {
-            ribCount ++
-            if(read in aln)
+            allCount ++
+            if((read in rib) && (read in gen))
             {
-               alnCount ++
+               inxCount ++
             }
-            else if(read in unl)
+            else if((read in rib) && !(read in gen))
             {
-               unlCount ++
+               ribCount ++
+            }
+            else if(!(read in rib) && (read in gen))
+            {
+               genCount ++
             }
             else
             {
                nulCount ++
             }
         }
-        print ribCount "\t" alnCount "\t" unlCount "\t" nulCount "\t"
+        ribCheck = 0
+        for(read in rib)
+        {
+            ribCheck ++
+        }
+        
+        genCheck = 0
+        for(read in gen)
+        {
+            genCheck ++
+        }
+         
+        print allCount "\t" genCount "\t" inxCount "\t" ribCount "\t"\
+              nulCount "\t" ribCheck "\t" genCheck
     }'\
-     <(samtools view -f67 ${ALIGNDIR}/${genomic})\
-     <(samtools view -f4 ${ALIGNDIR}/${genomic})\
-     <(samtools view -f67 -F256 ${ALIGNDIR}/${ribosomal})\
+     <(samtools view ${ALIGNDIR}/${genomic})\
+     <(samtools view -F4 ${ALIGNDIR}/${genomic})\
+     <(samtools view -F4 ${ALIGNDIR}/${ribosomal})\
      >${RSEQCDIR}/${ID}_ribosomal_reads_in_genomic.txt
+
+# genomic=${ID}_sorted_alignment.bam
+# ribosomal=${ID}_sorted_ribo.bam
+
+# gawk -v FS="\t"\
+#      'BEGIN {rc=0; fc=0}
+#      (rc > FNR)\
+#      {
+#         fc ++
+#         if(fc == 1)
+#         {
+#            unl[$1]
+#         }
+#         else
+#         {
+#            rib[$1]
+#         }
+#      }
+#     {rc = FNR - 1}
+#     (fc == 0){aln[$1]}	
+#     (fc == 1){unl[$1]}	
+#     (fc == 2){rib[$1]}	
+    
+#     END\
+#     {
+#         alnCount = 0
+#         unlCount = 0
+#         ribCount = 0
+#         nulCount = 0
+
+#         for(read in rib)
+#         {
+#             ribCount ++
+#             if(read in aln)
+#             {
+#                alnCount ++
+#             }
+#             else if(read in unl)
+#             {
+#                unlCount ++
+#             }
+#             else
+#             {
+#                nulCount ++
+#             }
+#         }
+#         print ribCount "\t" alnCount "\t" unlCount "\t" nulCount "\t"
+#     }'\
+#      <(samtools view -f67 ${ALIGNDIR}/${genomic})\
+#      <(samtools view -f4 ${ALIGNDIR}/${genomic})\
+#      <(samtools view -f67 -F256 ${ALIGNDIR}/${ribosomal})\
+#      >${RSEQCDIR}/${ID}_ribosomal_reads_in_genomic.txt
+# Subtract Ribosomal Reads from Genomic Alignments
+
+echo "removing reads aligned in" ${ribosomal} "from" ${genomic}
+echo 
+
+gawk '\
+     (NR == FNR) {tag[$1]; next}
+     (NR != FNR && $1 ~/^@/){print $0; next}
+     (NR != FNR && !($1 in tag)){print $0}
+     '\
+     <(samtools view -F4 ${ALIGNDIR}/${ribosomal})\
+     <(samtools view -h ${ALIGNDIR}/${genomic})\
+     | samtools view -b -o ${ALIGNDIR}/${ID}_ribo_filtered_alignment.bam
+
+# Index filtered bam File
+samtools index ${ALIGNDIR}/${ID}_ribo_filtered_alignment.bam
+
+# Generate Counts from Ribo-stripped alignments
+if [ -f ${COUNTDIR}/${ID}_RiboFilterGeneCount.txt ]
+then
+    echo skipping htseq-count for ${ID}_ribo_filtered_alignment.bam
+else
+    echo counting ${ID}_ribo_filtered_alignment.bam
+    htseq-count \
+	-i gene_id -r pos -f bam -s reverse -m union --type exon \
+	${ALIGNDIR}/${ID}_ribo_filtered_alignment.bam \
+	$GTFPATH > ${COUNTDIR}/${ID}_RiboFilterGeneCount.txt
+fi
+
+
+
